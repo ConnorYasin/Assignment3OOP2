ec9e6cdebc4233674d3684f6cb091ba5c031db12
David Cadena <68673038+Dav1dCad3na@users.noreply.github.com>
2025-04-15 15:10:21 -0600

    Added test cases, they neeed testing

--- Assignment3.Tests/LinkedListTest.cs
+++ Assignment3.Tests/LinkedListTest.cs
@@ -35,43 +35,70 @@ namespace Assignment3.Tests
         }
 
         [Test]
-        public void TestRemoveNodeBeginList()
+        public void TestRemoveFirstNode()
         {
-            list.AddFirst("John Smith");
+            User newUser = new User(1, "Joe Blow", "jblow@gmail.com", "password");
+            list.AddFirst(newUser)
             list.RemoveFirst();
-            Assert.IsNull(list.Head);
+            Assert.AreEqual(newUser, list.Head.Data);
         }
 
         [Test]
-        public void TestRemoveNodeEndList()
+        public void TestRemoveLastNode()
         {
-            list.AddLast("Jane Doe");
+            User newUser = (new User(4, "Ronald McDonald", "burgers4life63@outlook.com", "mcdonalds999"));
+            list.AddLast(newUser)
             list.RemoveLast();
-            Assert.IsNull(list.Tail);
+            Assert.IsNull(newUser, list.Tail)
         }
 
         [Test]
         public void TestValueofNode()
         {
-            list.AddLast("Bob Bobberson");
-            list.AddLast("Sam Sammerson");
-            list.AddLast("Dave Daverson");            
-            list.AddLast("Sammy Sammerson");
-            list.AddLast("Davey Daverson");
-
-            var node = list.Head?.Next?.Next;
-
-            Assert.AreEqual("Dave Daverson", node?.Data);
+            User node = list.Head?.Next?.Next;
+            Assert.AreEqual(User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555")););
         }
 
         [Test]
         public void TestSizeOfList()
         {
-            list.AddFirst("Bob Bobberson");
-            list.AddFirst("Sam Sammerson");
-            list.AddFirst("Dave Daverson");
-
-            Assert.AreEqual(3, list.count);
+            Assert.AreEqual(4, list.count);
         }
-    }
+        [Test]
+        public void TestContainsUser()
+        {
+            User result = list.Contains(new User(1, "Joe Blow", "jblow@gmail.com", "password"));
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void TestIndexOfUser()
+        {
+            User index = list.IndexOf(new User(3, "Colonel Sanders", "chickenlover1890@gmail.com", "kfc5555"));
+            Assert.AreEqual(2, index);
+        }
+
+        [Test]
+        public void TestReplaceUser()
+        {
+            User replacement = new User(10, "Updated", "update@me.com", "pass");
+            list.Replace(replacement, 1);
+            User updated = list.GetValue(1);
+            Assert.AreEqual(replacement, updated);
+        }
+
+        [Test]
+        public void TestClearList()
+        {
+            list.Clear();
+            Assert.IsTrue(list.IsEmpty());
+        }
+
+        [Test]
+        public void TestSortListByName()
+        {
+            list.SortListByName();
+            Assert.AreEqual("Colonel Sanders", list.GetFirst().Name);
+            Assert.AreEqual("Ronald McDonald", list.GetLast().Name);
+        }
 }
